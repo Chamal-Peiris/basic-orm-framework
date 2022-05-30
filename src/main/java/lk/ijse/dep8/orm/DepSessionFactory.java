d14a6845583c0b233d7233fb26b3533786553798
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-30 22:40:43 +0530

    Finalize the README and LICENSE files

--- src/main/java/lk/ijse/dep8/orm/DepSessionFactory.java
+++ src/main/java/lk/ijse/dep8/orm/DepSessionFactory.java
@@ -1,4 +1,7 @@
-package lk.ijse.dep8.orm.annotations;
+package lk.ijse.dep8.orm;
+
+import lk.ijse.dep8.orm.annotations.Entity;
+import lk.ijse.dep8.orm.annotations.Id;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -8,10 +11,24 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * DepSessionfactory is the starting porn of the ORM
+ *
+ * @author chamal-peiris
+ * @since 1.0.0
+ */
+
 
 public class DepSessionFactory {
     private final List<Class<?>> entityClassList = new ArrayList<>();
     private Connection connection;
+
+    /**
+     *Add classes that have benn annotated with <code>@Entity</code> annotation
+     * @param entityClass
+     * @return DepSessionFactory
+     * @throws RuntimeException if the class is not annotated with <code>@Entity</code> annotation
+     */
 
     public DepSessionFactory addAnnotatedClass(Class<?> entityClass){
         if (entityClass.getDeclaredAnnotation(Entity.class) == null){
@@ -20,12 +37,22 @@ public class DepSessionFactory {
         entityClassList.add(entityClass);
         return this;
     }
-
+    /**
+     * Set the connection
+     * @return connection set to initialize jdbc connection
+     * @return DepSessionFactory
+     */
     public DepSessionFactory setConnection(Connection connection){
         this.connection = connection;
         return this;
     }
 
+    /**
+     * validate whether eberything works okay
+     *
+     * @return DepSessionFactory
+     * @throws RuntimeException
+     */
     public DepSessionFactory build(){
         if (this.connection == null){
             throw new RuntimeException("Failed to build without a connection");
@@ -33,6 +60,11 @@ public class DepSessionFactory {
         return this;
     }
 
+    /**
+     * Bootstrap the ORM framework and create tables
+     *
+     * @throws SQLException
+     */
     public void bootstrap() throws SQLException {
         for (Class<?> entity : entityClassList) {
             String tableName = entity.getDeclaredAnnotation(Entity.class).value();
--- src/test/java/lk/ijse/dep8/orm/annotations/DepSessionFactoryTest.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.dep8.orm.annotations;
-
-
-
-class DepSessionFactoryTest {
-
-}
