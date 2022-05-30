0cc8935c02648c5b71fefd3cdad9cf7c312a51e7
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-05-30 19:51:47 +0530

    implementation of builder design pattern

--- src/main/java/lk/ijse/dep8/orm/annotations/Entity.java
+++ src/main/java/lk/ijse/dep8/orm/annotations/Entity.java
@@ -1,4 +1,16 @@
 package lk.ijse.dep8.orm.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Entity {
+
+    String value() default "";
+
+
+
 }
--- src/main/java/lk/ijse/dep8/orm/annotations/Id.java
+++ src/main/java/lk/ijse/dep8/orm/annotations/Id.java
@@ -1,4 +1,11 @@
 package lk.ijse.dep8.orm.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Id {
 }
